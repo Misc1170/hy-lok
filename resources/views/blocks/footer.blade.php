7abaaee54f1936b8d54ab0c021dc287438efc05e
lavreek <lavreeksolacki@yandex.ru>
2023-02-03 16:24:15 +0300

    Block and SASS loader

---
+++ resources/views/blocks/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer-logo">
+        <a>
+            <img src="/images/logo-white.png" alt="LOGO FOOTER">
+        </a>
+    </div>
+    <div class="footer-items">
+        <a href="mailto:mail@fluid-line.ru">mail@fluid-line.ru</a>
+        <a href="tel:+74959844100">+7 (495) 984-41-00</a>
+    </div>
+</footer>
---
+++ resources/views/index/blocks/catalogue.blade.php
@@ -0,0 +1,76 @@
+<div class="catalogue-container">
+    <div class="catalogue-title">
+        <p>КАТАЛОГ ПРОДУКЦИИ HY-LOK</p>
+    </div>
+
+    <div class="catalog">
+        <div class="catalog-item">
+            <a style="" href="/">
+                <img src="images/ball.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">КРАНЫ ШАРОВЫЕ </p>
+            <ul class="describe-catalog-item">
+                <p>Назначение шаровых кранов Hy-Lok:</p>
+                <li>Интрументальные</li>
+                <li>На большое ДУ</li>
+                <li>Креогенные</li>
+            </ul>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/fittingo.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">ФИТИНГИ ДЛЯ ТРУБ</p>
+            <p class="describe-catalog-item">Назначение шаровых кранов Hy-Lok: - Интрументальные - На большое ДУ - Креогенные</p>
+            <a class="more-catalog-item" href="/">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/membrano.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">МЕМБРАННЫЕ</p>
+            <p class="describe-catalog-item">Назначение шаровых кранов Hy-Lok: - <br>Интрументальные - На большое ДУ - Креогенные</p>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/valves_2.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">КРАНЫ ШАРОВЫЕ ИНТРУМЕНТАЛЬНЫЕ</p>
+            <p class="describe-catalog-item">Назначение шаровых кранов Hy-Lok: - Интрументальные - На большое ДУ - Креогенные</p>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/relefo.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">ПРЕД&shy;ОХРАНИТЕЛЬ&shy;НЫЕ КЛАПАНЫ</p>
+            <p class="describe-catalog-item">Назначение шаровых кранов Hy-Lok: - Интрументальные - На большое ДУ - Креогенные</p>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/checko.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">ФИЛЬТРЫ МИКРОННЫЕ ИНТРУМЕНТАЛЬНЫЕ</p>
+            <p class="describe-catalog-item">Назначение шаровых кранов Hy-Lok: - Интрументальные - На большое ДУ - Креогенные</p>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/dozirovka.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">ДОЗИРОВОЧНЫЕ ВЕНТИЛИ</p>
+            <p class="describe-catalog-item">Назначение шаровых кранов Hy-Lok: - Интрументальные - На большое ДУ - Креогенные</p>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+        <div class="catalog-item">
+            <a href="/">
+                <img src="images/manifoldo.png" alt="">
+            </a>
+            <p class="head-name-catalog-item">БОЛЬШЕ ПРОДУКЦИИ HY-LOK</p>
+            <a class="more-catalog-item" href="#">ПОДРОБНЕЕ</a>
+        </div>
+    </div>
+</div>
