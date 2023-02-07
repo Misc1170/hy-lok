2924678839cad994cd3d622b5205075330568cfa
Misc1170 <sloopy1170@gmail.com>
2023-02-07 14:11:18 +0300

    NotDone Adaptive

--- resources/views/index/blocks/banner.blade.php
+++ resources/views/index/blocks/banner.blade.php
@@ -1,11 +1,8 @@
-<div class="container">
-    <div class="row">
-        <div class="banner-with-text">
-            <div class="text-on-banner">
-                <h1>Чтобы Вы остались <br>довольны, мы создаем <br>ценность благодаря <br>
-                    производительности, <br>ноу-хау и быстрому <br>обслуживанию
-                </h1>
-            </div>
+
+    <div class="banner-with-text">
+        <div class="text-on-banner">
+            <h1>Чтобы Вы остались <br>довольны, мы создаем <br>ценность благодаря <br>
+                производительности, <br>ноу-хау и быстрому <br>обслуживанию
+            </h1>
         </div>
     </div>
-</div>
